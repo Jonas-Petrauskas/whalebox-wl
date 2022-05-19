adb51061605bd24ff101443785100b5d9c9ca29a
Jonas-Petrauskas <tiesiogbicas@gmail.com>
2022-05-19 15:41:20 +0200

    backend table delete and create fix, access code filter done

--- client/src/components/form/formComponent.jsx
+++ client/src/components/form/formComponent.jsx
@@ -1,12 +1,14 @@
 import "./formComponent.scss";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import CountdownTimer from "../countDownTimer/countDownComponent";
 import moment from "moment";
 import Stats from "../statistics/statsComponent";
+import PopUp from "../popupComponent/popupComponent";
 
 import { Amplify, API, graphqlOperation } from "aws-amplify";
-import { createTable } from "../../graphql/mutations";
+import { createTable, deleteTable } from "../../graphql/mutations";
+import { listTables } from "../../graphql/queries";
 
 import awsExports from "../../aws-exports";
 Amplify.configure(awsExports);
@@ -16,6 +18,29 @@ const id = Math.random().toString(36).substring(2);
 
 function FormComponent() {
   const [count, setCount] = useState(0);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const fetchItems = async () => {
+    try {
+      const itemsData = await API.graphql(graphqlOperation(listTables));
+      const itemsList = itemsData.data.listTables.items;
+      setItems(itemsList);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  let codeList = [];
+
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].code) {
+      codeList.push(items[i]);
+    }
+  }
 
   const incrementCount = () => {
     setCount(count + 1);
@@ -29,14 +54,32 @@ function FormComponent() {
         accessCode: data.accessCode,
         timestamp: dateNow,
       };
-      await API.graphql(graphqlOperation(createTable, { input: item }));
-      reset({
-        walletAddess: "",
-        accessCode: "",
-      });
-      setTimeout(() => {
-        window.location.reload(false);
-      }, 500);
+      if (codeList.length > 0) {
+        for (let i = 0; i < codeList.length; i++) {
+          if (codeList[i].code === item.accessCode) {
+            await API.graphql(graphqlOperation(createTable, { input: item }));
+
+            const idObject = {
+              id: codeList[i].id,
+            };
+
+            await API.graphql(
+              graphqlOperation(deleteTable, { input: idObject })
+            );
+            reset({
+              walletAddess: "",
+              accessCode: "",
+            });
+            setTimeout(() => {
+              window.location.reload(false);
+            }, 500);
+          } else {
+            console.log("code not valid");
+          }
+        }
+      } else {
+        console.log("code not valid");
+      }
     } catch (err) {
       console.log("error creating todo:", err);
     }
@@ -51,6 +94,7 @@ function FormComponent() {
 
   return (
     <div>
+      <PopUp />
       <form onSubmit={handleSubmit(createItem)}>
         <div className="form-timer">
           <CountdownTimer />
